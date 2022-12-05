b32d4e4a183afeec05c3c9addecf2dbf83f572bd
Gonzo909 <gonzalo90952@gmail.com>
2022-12-05 16:08:43 -0700

    finished program

--- Jeff.java
+++ Jeff.java
@@ -16,27 +16,29 @@ public class Jeff {
         System.out.println(player2.getHand());
         System.out.println("");
 
+        //PLAYER 1 choose opponents 2 cards
+        Scanner getChoice = new Scanner(System.in);
+
+        System.out.println("HIDE CARDS PHASE");
+        System.out.println("Player 1, please choose first card to hide (1, 2, 3, 4)");
+        int firstHidden1 = getChoice.nextInt();
+
+        System.out.println("Please choose second card to hide (1, 2, 3, 4)");
+        int secondHidden1 = getChoice.nextInt();
+
+        player2.getHiddenHand(firstHidden1, secondHidden1);
+
+        //PLAYER 2 choose opponents 2 cards
+        System.out.println("Player 2, please choose first card to hide (1, 2, 3, 4)");
+        int firstHidden2 = getChoice.nextInt();
+
+        System.out.println("Please choose second card to hide (1, 2, 3, 4)");
+        int secondHidden2 = getChoice.nextInt();
+
+        player1.getHiddenHand(firstHidden2, secondHidden2);
+
+
         while (deck.getSize() > 0) {
-            //PLAYER 1 choose opponents 2 cards
-            Scanner getChoice = new Scanner(System.in);
-
-            System.out.println("HIDE CARDS PHASE");
-            System.out.println("Player 1, please choose first card to hide (1, 2, 3, 4)");
-            int firstHidden1 = getChoice.nextInt();
-
-            System.out.println("Please choose second card to hide (1, 2, 3, 4)");
-            int secondHidden1 = getChoice.nextInt();
-
-            player2.getHiddenHand(firstHidden1, secondHidden1);
-
-            //PLAYER 2 choose opponents 2 cards
-            System.out.println("Player 2, please choose first card to hide (1, 2, 3, 4)");
-            int firstHidden2 = getChoice.nextInt();
-
-            System.out.println("Please choose second card to hide (1, 2, 3, 4)");
-            int secondHidden2 = getChoice.nextInt();
-
-            player1.getHiddenHand(firstHidden2, secondHidden2);
 
             //battle phase
             System.out.println("BATTLE PHASE");
@@ -57,8 +59,10 @@ public class Jeff {
 
             //add cards to bank
             if (p1BattleCard.getPoint_value() >= p2BattleCard.getPoint_value()) {
+                System.out.println("Player One Wins!!");
                 player1.addToBank(p2BattleCard);
             } else {
+                System.out.println("Player Two Wins!!");
                 player2.addToBank(p1BattleCard);
             }
 
